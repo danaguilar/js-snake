316eeebda98c3fb83d727de057da163422fd5c64
Daniel <danaguilar6@gmail.com>
2017-07-07 14:33:53 -0500

    added interactivity with the player circle

--- index.js
+++ index.js
@@ -3,15 +3,74 @@ $(document).ready(function() {
   var ch = $(".container").height();
   var createGrid = function(cw, ch){
     var $container = $(".container")
-    for(i = 0; i <= cw/10; i++){
+    for (i = 0; i < ch/20; i++){
       var $newRow = $("<div class = 'row'></div>");
-      for(i = 0; i <= ch/10; i++){
-        $newRow.append("<div class = 'square'></div>");
-        console.log("appended square");
+      for (j = 0; j < cw/20; j++){
+        $newRow.append("<div class = 'square' id = r" + i + "c"+ j + "></div>");
       };
       $container.append($newRow);
     };
   };
-  console.log("starting creategrid");
+  var makeActive = function(row, column){
+    var id = "#r" +row + "c " + column;
+    console.log(id);
+    $(id).addClass('active');
+  };
+
+  var player = {
+    BGcolor : "green",
+    row : 2,
+    col : 3,
+    draw : function(){
+      var $square = $("#r" + this.row + "c" + this.col);
+      $square.css("background-color", this.BGcolor);
+      $square.addClass('active');
+    },
+    remove : function(){
+      var $square = $("#r" + this.row + "c" + this.col);
+      $square.css("background-color", "transparent");
+      $square.removeClass('active');
+    },
+    moveLeft : function(){
+      this.remove();
+      this.col -= 1;
+      this.draw();
+    },
+    moveRight : function(){
+      this.remove();
+      this.col += 1;
+      this.draw();
+    },
+    moveUp : function(){
+      this.remove();
+      this.row -= 1;
+      this.draw();
+    },
+    moveDown : function(){
+      this.remove();
+      this.row += 1;
+      this.draw();
+    },
+  };
   createGrid(cw,ch);
+  player.draw();
+  $(document).on('keydown',(function (event){
+    switch(event.which){
+      case 38:
+        console.log("moving up");
+        player.moveUp();
+        break;
+      case 40:
+        player.moveDown();
+        break;
+      case 37:
+        player.moveLeft();
+        break;
+      case 39:
+        player.moveRight();
+        break;
+      default:
+        break;
+    }
+  }));
 });
